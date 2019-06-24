df815e5c24a2e901380a89d77a2054753f79f5a0
Seth Shaw <seth.shaw@unlv.edu>
2019-06-24 11:16:49 -0700

    update archivesspace notes process plugin + added abstract and userestrict to resource

--- src/Plugin/migrate/process/ArchivesSpaceNotes.php
+++ src/Plugin/migrate/process/ArchivesSpaceNotes.php
@@ -27,38 +27,34 @@ class ArchivesSpaceNotes extends ProcessPluginBase {
     if (!is_array($value)) {
       throw new MigrateException(sprintf('ArchivesSpace Notes process failed for destination property (%s): input is not an array.', $destination_property));
     }
+    if (empty($this->configuration['type']) && !array_key_exists('type', $this->configuration)) {
+      throw new MigrateException('ArchivesSpace Notes plugin is missing type configuration.');
+    }
 
-    // Start with the easy attributes.
-    $label = (isset($value['lable'])) ? $value['label'] : '';
-    $type = (isset($value['type'])) ? $value['type'] : 'odd';
-    $content = '';
+    if ($value['type'] === $this->configuration['type'] && $value['publish'] === TRUE) {
+      $content = '';
+      switch ($value['jsonmodel_type']) {
+        case 'note_singlepart':
+          foreach ($value['content'] as $line) {
+            $content .= "<p>$line</p>";
+          }
+          break;
 
-    // Single or multipart notes?
-    if ($value['jsonmodel_type'] == 'note_singlepart') {
-      foreach ($value['content'] as $p) {
-        $content .= "<p>$p</p>";
+        case 'note_multipart':
+          foreach ($value['subnotes'] as $subnote) {
+            if ($subnote['publish'] === TRUE) {
+              $content .= '<p>' . $subnote['content'] . '</p>';
+            }
+          }
+          break;
+      }
+      if (!empty($content)) {
+        return [
+          'value' => $content,
+          'format' => 'basic_html',
+        ];
       }
     }
-    elseif ($value['jsonmodel_type'] == 'note_multipart') {
-      foreach ($value['subnotes'] as $subnote) {
-        if ($subnote['publish']) {
-          $content .= '<p>' . $subnote['content'] . '</p>';
-        }
-      }
-    }
-
-    // Is the note published and does it contain content?
-    if (isset($value['publish']) && $value['publish'] && $content) {
-      return [
-        'label' => $label,
-        'note_type' => $type,
-        'note' => $content,
-      ];
-    }
-    else {
-      throw new MigrateSkipProcessException();
-    }
-
   }
 
 }
